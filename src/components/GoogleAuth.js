85651182d08786cff8446991274ba0b2cd482f78
atotared <atota@red.software.systems>
2021-05-19 18:05:32 +0200

    Implement basic OAuth functionality

--- src/components/GoogleAuth.js
+++ src/components/GoogleAuth.js
@@ -1,18 +1,33 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 
 const GoogleAuth = () => {
+  const [isSignedIn, setIsSignedIn] = useState(null);
+
   useEffect(() => {
     // Load Google API library
     window.gapi.load("client:auth2", () => {
-      // Initialize the library
-      window.gapi.client.init({
-        clientId:
-          "322059734492-bj8ljm3aboh7echc6kcvgep2uknhnai9.apps.googleusercontent.com",
-        scope: "email",
-      });
+      // Initialize the library - init returns a Promise
+      window.gapi.client
+        .init({
+          clientId:
+            "322059734492-bj8ljm3aboh7echc6kcvgep2uknhnai9.apps.googleusercontent.com",
+          scope: "email",
+        })
+        .then(() => {
+          const auth = window.gapi.auth2.getAuthInstance();
+          setIsSignedIn(auth.isSignedIn.get());
+        });
     });
   }, []);
-  return <div>Google Auth</div>;
+
+  const renderAuthButton = () => {
+    if (isSignedIn === null) {
+      return <div>Don't know if we're signed in</div>;
+    }
+    return isSignedIn ? <div>I'm signed in</div> : <div>Not signed in</div>;
+  };
+
+  return <div>{renderAuthButton()}</div>;
 };
 
 export default GoogleAuth;
