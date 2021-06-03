ae556c877c45ac0db3a585a0ac01bcb8109f575d
atotared <atota@red.software.systems>
2021-06-03 22:27:23 +0200

    Wire up redux-form

--- src/store/reducers/index.js
+++ src/store/reducers/index.js
@@ -1,6 +1,8 @@
 import { combineReducers } from "redux";
+import { reducer as reduxFormReducer } from "redux-form";
 import authReducer from "./auth";
 
 export default combineReducers({
   auth: authReducer,
+  form: reduxFormReducer,
 });
