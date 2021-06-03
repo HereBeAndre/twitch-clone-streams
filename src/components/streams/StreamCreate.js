05e63de7094ad83783d19f20f6b7ae10bdaa8df2
atotared <atota@red.software.systems>
2021-06-03 23:05:29 +0200

    Initialize redux-form

--- src/components/streams/StreamCreate.js
+++ src/components/streams/StreamCreate.js
@@ -1,5 +1,28 @@
+import { Field, reduxForm } from "redux-form";
+
 const StreamCreate = () => {
-  return <div>StreamCreate</div>;
+  const renderInput = (formProps) => {
+    console.log(formProps);
+    return (
+      <input
+        {...formProps.input} // TAKES ALL PROPERTIES OF input OBJECT AND ASSIGNS THEM TO <input /> AS props
+        // EQUIVALENT TO THIS:
+        // onChange={formProps.input.onChange}
+        // value={formProps.input.value}
+      />
+    );
+  };
+
+  return (
+    <div>
+      <form>
+        <Field name="title" component={renderInput} />
+        <Field name="description" component={renderInput} />
+      </form>
+    </div>
+  );
 };
 
-export default StreamCreate;
+export default reduxForm({
+  form: "streamCreate",
+})(StreamCreate);
