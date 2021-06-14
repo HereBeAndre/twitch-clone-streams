0609ca907e2abf0674fa4a3babc6eb20a3207c37
atotared <atota@red.software.systems>
2021-06-14 23:22:30 +0200

    Set up streams reducer

--- src/store/reducers/index.js
+++ src/store/reducers/index.js
@@ -1,10 +1,10 @@
 import { combineReducers } from "redux";
 import { reducer as reduxFormReducer } from "redux-form";
 import authReducer from "./auth";
-import streamReducer from "./streams";
+import streamsReducer from "./streams";
 
 export default combineReducers({
   auth: authReducer,
   form: reduxFormReducer,
-  stream: streamReducer,
+  streams: streamsReducer,
 });
--- src/store/reducers/streams/index.js
+++ src/store/reducers/streams/index.js
@@ -1,9 +1,11 @@
+import _ from "lodash";
 import types from "../../actions/types_d";
 
+// eslint-disable-next-line import/no-anonymous-default-export
 export default (state = {}, action) => {
   switch (action.type) {
-    case types.GET_SET_ALL_STREAMS:
-      return {};
+    case types.GET_ALL_STREAMS:
+      return { ...state, ..._.mapKeys(action.payload, "id") };
     case types.SET_NEW_STREAM:
       return { ...state, [action.payload.id]: action.payload };
     case types.GET_STREAM:
@@ -11,7 +13,7 @@ export default (state = {}, action) => {
     case types.SET_UPDATE_STREAM:
       return { ...state, [action.payload.id]: action.payload };
     case types.SET_DELETE_STREAM:
-      return {};
+      return _.omit(state, action.payload);
     default:
       return state;
   }
