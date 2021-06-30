5523239dfbe81ec135af85287a7fd684dbf9f398
atotared <atota@red.software.systems>
2021-06-30 08:20:49 +0200

    Add stream show view

--- src/components/streams/StreamShow.js
+++ src/components/streams/StreamShow.js
@@ -1,5 +1,31 @@
-const StreamShow = () => {
-  return <div>StreamShow</div>;
+import { useEffect } from "react";
+import { useDispatch } from "react-redux";
+import { connect } from "react-redux";
+import { getStream } from "../../store/actions";
+
+const StreamShow = (props) => {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(getStream(props.match.params.id));
+  }, []);
+
+  return (
+    <>
+      {!props.stream ? (
+        <div>Loading...</div>
+      ) : (
+        <div>
+          <h1>{props.stream.title}</h1>
+          <h5>{props.stream.description}</h5>
+        </div>
+      )}
+    </>
+  );
 };
 
-export default StreamShow;
+const mapStateToProps = (state, ownProps) => {
+  return { stream: state.streams[ownProps.match.params.id] };
+};
+
+export default connect(mapStateToProps, { getStream })(StreamShow);
